ef93b6b26cf208ba361202f1961283fa7e38f2e1
Gareth <oogarethrice@gmail.com>
2024-04-29 20:32:06 -0700

    Add about and shell of popup modal

--- Scripts/sub-projectInteract.js
+++ Scripts/sub-projectInteract.js
@@ -1,4 +1,17 @@
-function toggleVisDiceTray(){
-    var popupA = document.getElementById("popDiceTray"); 
-    popupA.classList.toggle("show"); 
+//dice modal
+var diceModal = document.getElementById("dice-modal"); 
+
+var img = document.getElementById("dice-modal-img"); 
+var modalImg = document.getElementById("img01"); 
+var captionText = document.getElementById("caption"); 
+img.onclick = function(){
+    diceModal.style.display = "block"; 
+    modalImg.src = this.src; 
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0]; 
+
+span.onclick = function(){
+    diceModal.style.display = "none"; 
 }
